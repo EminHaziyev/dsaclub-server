f46ce357d279b32f9c47f9ad9cd516a4eef030d8
Həziyev Emin <mn05x.mail@gmail.com>
2025-08-11 22:05:16 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+
+
+const GITHUB_OWNER = "EminHaziyev";
+const GITHUB_REPO = "DsaClub-Peerstack";
+const BRANCH = "main"; 
+
+
+app.get("/", async (req, res) => {
+  try {
+    const url = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents`;
+    const { data } = await axios.get(url);
+
+    const folders = data
+      .filter(item => item.type === "dir" && item.name)
+      .map(item => item.name);
+
+    res.json(folders);
+  } catch (err) {
+    res.status(500).json({ error: "Cannot fetch weeks list", details: err.message });
+  }
+});
+
+
+app.get("/week/:name", async (req, res) => {
+  try {
+    const folderName = req.params.name;
+    const url = `https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/${BRANCH}/${folderName}/data.json`;
+
+    const { data } = await axios.get(url);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Cannot fetch week data", details: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
