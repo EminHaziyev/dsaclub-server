0b65280f14014c1c82c026b8615bdb1997b1b1e4
Həziyev Emin <mn05x.mail@gmail.com>
2025-08-13 12:13:51 +0400

    Solved a bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 
@@ -8,41 +8,51 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 const GITHUB_OWNER = "EminHaziyev";
 const GITHUB_REPO = "DsaClub-Peerstack";
-const BRANCH = "main"; 
+const BRANCH = "main";
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.get("/", async (req, res) => {
   try {
     const url = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents`;
     const { data } = await axios.get(url, {
-  headers: {
-    'Authorization': `token ${process.env.GITHUB_TOKEN}`,
-    'User-Agent': 'EminHaziyev-App'
-  }
-});
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        "User-Agent": "EminHaziyev-App",
+      },
+    });
 
-    
     const folders = data
-      .filter(item => item.type === "dir" && item.name)
-      .map(item => item.name);
+      .filter((item) => item.type === "dir" && item.name)
+      .map((item) => item.name);
 
-    res.render('dsa', { folders });
+    res.render("dsa", { folders });
   } catch (err) {
-    res.status(500).json({ error: err.message, details: err.response ? err.response.data : "No response from server" });
+    res
+      .status(500)
+      .json({
+        error: err.message,
+        details: err.response ? err.response.data : "No response from server",
+      });
   }
 });
-
 
 app.get("/week/:name", async (req, res) => {
   try {
     const folderName = req.params.name;
     const url = `https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/${BRANCH}/${folderName}/data.json`;
 
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        "User-Agent": "EminHaziyev-App",
+      },
+    });
     // res.json(data);
-    res.render('week', { folder: folderName, data });
+    res.render("week", { folder: folderName, data });
   } catch (err) {
-    res.status(500).json({ error: "Cannot fetch week data", details: err.message });
+    res
+      .status(500)
+      .json({ error: "Cannot fetch week data", details: err.message });
   }
 });
 
